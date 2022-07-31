d20f5626e15c2b0741972459d43208c0055f9e8a
vuongbachdoan <vuongbachdoan@gmail.com>
2022-07-31 10:27:28 +0700

    fix customerUpdate UI

--- src/hackathon_coursework_frontend/src/components/spec/CustomerUpdate.jsx
+++ src/hackathon_coursework_frontend/src/components/spec/CustomerUpdate.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
 
 export default function CustomerUpdate() {
     return (
-        <div>
+        <Container>
             <h1>Update</h1>
             <div class="input-group flex-nowrap">
                 <span class="input-group-text" id="addon-wrapping">First name</span>
@@ -12,12 +13,16 @@ export default function CustomerUpdate() {
                 <span class="input-group-text" id="addon-wrapping">Last name</span>
                 <input type="text" class="form-control" placeholder="Last name" aria-label="Username" aria-describedby="addon-wrapping" />
             </div>
-            <div class="input-group flex-nowrap">
+            <div class="input-group flex-nowrap align-items-center">
                 <span class="input-group-text" id="addon-wrapping">Last name</span>
-                <label htmlFor="">Male</label>
-                <input type="radio" name="sex" value="male" id="" />
-                <label htmlFor="">Female</label>
-                <input type="radio" name="sex" value="female" id="" />
+                <div className="radio_btn">
+                    <label htmlFor="">Male</label>
+                    <input type="radio" name="sex" value="male" id="" />
+                </div>
+                <div className="radio_btn">
+                    <label htmlFor="">Female</label>
+                    <input type="radio" name="sex" value="female" id="" />
+                </div>
             </div>
             <div class="input-group flex-nowrap">
                 <span class="input-group-text" id="addon-wrapping">Date of birth</span>
@@ -31,8 +36,30 @@ export default function CustomerUpdate() {
                 <span class="input-group-text" id="addon-wrapping">Address</span>
                 <input type="text" class="form-control" placeholder="Last name" aria-label="Address" aria-describedby="addon-wrapping" />
             </div>
+            <div className="d-flex justify-content-center">
             <button type="button" class="btn btn-success">Save</button>
             <button type="button" class="btn btn-danger">Delete</button>
-        </div>
+            </div>
+        </Container>
     )
 }
+
+
+const Container = styled.div`
+    width: 800px;
+    margin: 0px calc((100% - 800px) / 2);
+    span{
+        width: 30%;
+    }
+    .radio_btn{
+        margin-right: 15px;
+        label{
+            margin-left: 15px;
+            color: #6c757d
+        }
+        input{
+            margin-left: 15px;
+            cursor: pointer;
+        }
+    }
+`;
